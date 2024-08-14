b77fafc76e3c3aba6e11d24c230baa233078c56e
Warsono <warsono16694@gmail.com>
2024-08-14 15:43:41 +0700

    chore: add app config

---
+++ app.config.ts
@@ -0,0 +1,6 @@
+export default defineAppConfig({
+  app: {
+    name: 'Acme App',
+    description: 'Nuxt Shadcn Starter',
+  },
+})
--- app.vue
+++ app.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+const appConfig = useAppConfig()
+
+useHead({
+  title: appConfig.app.name,
+  meta: [
+    {
+      name: 'description',
+      content: appConfig.app.description,
+    },
+  ],
+})
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLoadingIndicator />
