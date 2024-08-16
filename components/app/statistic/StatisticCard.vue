9d5e8cdbe4d547c011dab44c87bb305a6ed0afb6
Warsono <warsono16694@gmail.com>
2024-08-16 17:10:44 +0700

    feat: add `StatisticCard` component for displaying statistics

---
+++ components/app/statistic/StatisticCard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+export interface StatisticCardProps {
+  title: string
+  value: string | number
+  icon: string | Component
+  color: 'blue' | 'orange' | 'green' | 'red'
+}
+
+defineProps<StatisticCardProps>()
+
+const colorMap: Record<string, string> = {
+  blue: 'bg-blue-500 text-white',
+  orange: 'bg-orange-500 text-white',
+  green: 'bg-green-500 text-white',
+  red: 'bg-red-500 text-white',
+}
+</script>
+
+<template>
+  <div class="bg-white px-4 py-3 rounded-[12px] shadow flex gap-4 items-center">
+    <div
+      class="rounded-lg flex items-center justify-center w-[61px] h-[71px]"
+      :class="colorMap[color]"
+    >
+      <component :is="icon" class="size-7" />
+    </div>
+    <div class="flex flex-col">
+      <h3 class="text-sm font-medium text-gray-500">
+        {{ title }}
+      </h3>
+      <div class="text-[32px]/[48px] font-semibold text-gray-800">
+        {{ value }}
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,39 @@
 <script setup lang="ts">
-const tabs = ref([
-  { value: 'overview', label: 'Overview' },
-  { value: 'analytics', label: 'Analytics' },
-  { value: 'reports', label: 'Reports', disabled: true },
-  { value: 'notifications', label: 'Notifications', disabled: true },
-])
+import { CloseCircleIcon, WalletCheckIcon } from '@morphemeicons/vue/iconsax/bulk'
+import { NoteIcon, UserEditIcon } from '@morphemeicons/vue/iconsax/linear'
+import type { StatisticCardProps } from '~/components/app/statistic/StatisticCard.vue'
 
-const selectedTab = ref('overview')
+const statisticItems = ref<StatisticCardProps[]>([
+  {
+    title: 'Baru Diajukan',
+    value: 100,
+    icon: NoteIcon,
+    color: 'blue',
+  },
+  {
+    title: 'Sedang Ditinjau',
+    value: 100,
+    icon: UserEditIcon,
+    color: 'orange',
+  },
+  {
+    title: 'Disetujui',
+    value: 100,
+    icon: WalletCheckIcon,
+    color: 'green',
+  },
+  {
+    title: 'Ditolak',
+    value: 100,
+    icon: CloseCircleIcon,
+    color: 'red',
+  },
 
-const metrics = ref([
-  {
-    title: 'Total Revenue',
-    value: '$45,231.89',
-    percentage: '+20.1% from last month',
-    icon: 'ph:currency-dollar',
-  },
-  {
-    title: 'Subscriptions',
-    value: '+2350',
-    percentage: '+180.1% from last month',
-    icon: 'ph:users',
-  },
-  {
-    title: 'Sales',
-    value: '+12,234',
-    percentage: '+19% from last month',
-    icon: 'ph:credit-card',
-  },
-  {
-    title: 'Active Now',
-    value: '+573',
-    percentage: '+201 since last hour',
-    icon: 'ph:pulse',
-  },
 ])
 </script>
 
 <template>
-  <div class="flex-1 space-y-4">
+  <div class="flex-1 space-y-6">
     <div
       class="flex flex-col lg:flex-row gap-4 lg:items-center justify-between space-y-2"
     >
@@ -49,30 +45,21 @@ const metrics = ref([
         <Button>Download</Button>
       </div>
     </div>
-    <Tabs v-model="selectedTab" class="space-y-4">
-      <TabsList>
-        <TabsTrigger
-          v-for="tab in tabs"
-          :key="tab.value"
-          :value="tab.value"
-          :disabled="tab.disabled"
-        >
-          {{ tab.label }}
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent value="overview" class="space-y-4">
-        <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-          <MetricCard
-            v-for="metric in metrics"
-            :key="metric.title"
-            :metric="metric"
-          />
-        </div>
-        <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-          <OverviewCard />
-          <RecentSalesCard />
-        </div>
-      </TabsContent>
-    </Tabs>
+
+    <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+      <StatisticCard
+        v-for="statistic in statisticItems"
+        :key="statistic.title"
+        :title="statistic.title"
+        :value="statistic.value"
+        :icon="statistic.icon"
+        :color="statistic.color"
+      />
+    </div>
+
+    <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+      <OverviewCard />
+      <RecentSalesCard />
+    </div>
   </div>
 </template>
